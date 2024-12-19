00d0565a6c0b411968bac52ce9f684ff105b3ff8
Fegaso <85967636+Fegaso@users.noreply.github.com>
2024-12-19 11:10:21 -0300

    Finalizando o Projeto

--- core/tests/test_view_home.py
+++ core/tests/test_view_home.py
@@ -1,30 +1,39 @@
 from django.test import TestCase
+from django.shortcuts import resolve_url as r
+
 
 class HomeTest(TestCase):
+    fixtures = ['keynotes.json',]
+
     def setUp(self):
-        self.response = self.client.get('/')
+        self.response = self.client.get(r('home'))
+
     def test_get(self):
         '''
         Testa se a página inicial retorna status code 200
         '''
         self.assertEqual(200, self.response.status_code)
+
     def test_template(self):
         self.assertTemplateUsed(self.response, 'index.html')
-        
+
     def test_link_subscription(self):
-        self.assertContains(self.response, 'href="/inscricao/"')
+        self.assertContains(
+            self.response, 'href="{}"'.format(r('subscriptions:new')))
 
     def test_speakers(self):
         contents = [
             'Grace Hopper',
             'https://abre.ai/hopper-pic',
+            'href="{}"'.format(r('speaker_detail', slug='grace-hopper')),
             'Alan Turing',
-            'https://abre.ai/turing-pic'
+            'https://abre.ai/turing-pic',
+            'href="{}"'.format(r('speaker_detail', slug='alan-turing'))
         ]
         for expected in contents:
             with self.subTest():
                 self.assertContains(self.response, expected)
-                
+
     def test_speakers_link(self):
         expected = 'href="{}#speakers"'.format(r('home'))
         self.assertContains(self.response, expected)
--- core/tests/test_view_speaker_detail.py
+++ core/tests/test_view_speaker_detail.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from django.shortcuts import resolve_url as r
+
 from core.models import Speaker
+
+
 class SpeakerDetailGet(TestCase):
     def setUp(self):
         speaker = Speaker.objects.create(
@@ -11,10 +14,13 @@ class SpeakerDetailGet(TestCase):
             description='Programadora e almirante.'
         )
         self.resp = self.client.get(r('speaker_detail', slug=speaker.slug))
+
     def test_get(self):
         self.assertEqual(200, self.resp.status_code)
+
     def test_template(self):
         self.assertTemplateUsed(self.resp, 'core/speaker_detail.html')
+
     def test_html(self):
         contents = [
             'Grace Hopper',
@@ -25,9 +31,12 @@ class SpeakerDetailGet(TestCase):
         for expected in contents:
             with self.subTest():
                 self.assertContains(self.resp, expected)
+
     def test_context(self):
         speaker = self.resp.context['speaker']
         self.assertIsInstance(speaker, Speaker)
+
+
 class SpeakerDetailNotFound(TestCase):
     def test_not_found(self):
         resp = self.client.get(r('speaker_detail', slug='not-found'))
